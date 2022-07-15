12d9c88e04dd61a67bd5d35b1f003d32936eb318
Ayaka Mikazuki <ayaka@mail.shn.hk>
2022-07-15 12:46:08 +0800

    Remove duplicate entries

--- scripts/extract_word.py
+++ scripts/extract_word.py
@@ -1,12 +1,17 @@
 from itertools import chain
 from utils import split_pinyin_str
 
+d = {}
+
 with open('source/普通話詞語表（表一）.txt') as f1, \
-        open('source/普通話詞語表（表二）.txt') as f2, \
-        open('words.txt', 'w') as f3:
+        open('source/普通話詞語表（表二）.txt') as f2:
     for line in chain(f1, f2):
         word, pinyin_str = line.rstrip('\n').split('\t')
         word = word.lstrip('*')
         # pinyin_str = split_pinyin_str(pinyin_str)
         # print(word, pinyin_str)
-        print(word, file=f3)
+        d[word] = None
+
+with open('words.txt', 'w') as f:
+    for word in d:
+        print(word, file=f)
